7f42c69f47abcf826d047eb5f34103567470833e
Daniele Del Giudice <daniele@delgiudice.dev>
2021-10-27 12:41:26 +0000

    Solution for day 01

--- lib/mix/tasks/d01.p2.ex
+++ lib/mix/tasks/d01.p2.ex
@@ -5,7 +5,11 @@ defmodule Mix.Tasks.D01.P2 do
 
   @shortdoc "Day 01 Part 2"
   def run(args) do
-    input = nil
+    input =
+      "./inputs/day_01.txt"
+      |> File.stream!([], :line)
+      |> Stream.map(&String.trim/1)
+      |> Enum.map(&String.to_integer/1)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_2: fn -> input |> part2() end}),
